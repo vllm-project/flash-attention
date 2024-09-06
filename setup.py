3b5466d2f04786e67715ec97fd397f19691603d5
luka <luka@neuralmagic.com>
2024-09-06 14:08:49 -0400

    Skip install if same dir

--- setup.py
+++ setup.py
@@ -226,6 +226,10 @@ class cmake_build_ext(build_ext):
             # Install the extension into the proper location
             outdir = Path(self.get_ext_fullpath(ext.name)).parent.absolute()
 
+            # Skip if the install directory is the same as the build directory
+            if outdir == self.build_temp:
+                continue
+
             install_args = [
                 "cmake", "--install", ".", "--prefix", outdir, "--component",
                 target_name(ext.name)
@@ -242,27 +246,6 @@ def get_package_version():
         return f"{public_version}+{local_version}"
     else:
         return str(public_version)
-
-
-class NinjaBuildExtension(BuildExtension):
-    def __init__(self, *args, **kwargs) -> None:
-        # do not override env MAX_JOBS if already exists
-        if not os.environ.get("MAX_JOBS"):
-            import psutil
-
-            # calculate the maximum allowed NUM_JOBS based on cores
-            max_num_jobs_cores = max(1, os.cpu_count() // 2)
-
-            # calculate the maximum allowed NUM_JOBS based on free memory
-            free_memory_gb = psutil.virtual_memory().available / (1024 ** 3)  # free memory in GB
-            max_num_jobs_memory = int(free_memory_gb / 9)  # each JOB peak memory cost is ~8-9GB when threads = 4
-
-            # pick lower value of jobs based on cores vs memory metric to minimize oom and swap usage during compilation
-            max_jobs = max(1, min(max_num_jobs_cores, max_num_jobs_memory))
-            os.environ["MAX_JOBS"] = str(max_jobs)
-
-        super().__init__(*args, **kwargs)
-
 
 PYTORCH_VERSION = "2.4.0"
 MAIN_CUDA_VERSION = "12.1"

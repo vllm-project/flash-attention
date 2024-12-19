9be126f844bf7b5b22ae336f986dbca16dcb8958
Minmin Sun <minmin.smm@alibaba-inc.com>
2024-12-19 06:42:52 +0000

    Add sparse attention with virtical and slash

--- vllm_flash_attn/__init__.py
+++ vllm_flash_attn/__init__.py
@@ -3,6 +3,7 @@ __version__ = "2.6.2"
 # Use relative import to support build-from-source installation in vLLM
 from .flash_attn_interface import (
     flash_attn_func,
+    sparse_attn_func,
     flash_attn_varlen_func,
     flash_attn_with_kvcache,
 )

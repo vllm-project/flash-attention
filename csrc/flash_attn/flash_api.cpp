aa3f5d0d6da55a66d7f635571002f56cd628635b
Lu Fang <lufang@fb.com>
2025-01-13 23:10:05 -0800

    Replace c10::optional with std::optional in flash_attn
    Signed-off-by: Lu Fang <lufang@fb.com>

--- csrc/flash_attn/flash_api.cpp
+++ csrc/flash_attn/flash_api.cpp
@@ -242,7 +242,7 @@ std::tuple<at::Tensor, at::Tensor> set_params_splitkv(Flash_fwd_params &params, 
     return std::make_tuple(softmax_lse_accum, out_accum);
 }
 
-void set_params_alibi(Flash_fwd_params &params, const c10::optional<at::Tensor> &alibi_slopes_, int batch_size, int num_heads){
+void set_params_alibi(Flash_fwd_params &params, const std::optional<at::Tensor> &alibi_slopes_, int batch_size, int num_heads){
 #ifdef FLASHATTENTION_DISABLE_ALIBI
     TORCH_CHECK(!alibi_slopes_.has_value(), "This flash attention build does not support alibi.");
     params.alibi_slopes_ptr = nullptr;
@@ -265,8 +265,8 @@ std::vector<at::Tensor>
 mha_fwd(at::Tensor &q,         // batch_size x seqlen_q x num_heads x head_size
         const at::Tensor &k,         // batch_size x seqlen_k x num_heads_k x head_size
         const at::Tensor &v,         // batch_size x seqlen_k x num_heads_k x head_size
-        const c10::optional<at::Tensor> &out_,             // batch_size x seqlen_q x num_heads x head_size
-        const c10::optional<at::Tensor> &alibi_slopes_, // num_heads or batch_size x num_heads
+        const std::optional<at::Tensor> &out_,             // batch_size x seqlen_q x num_heads x head_size
+        const std::optional<at::Tensor> &alibi_slopes_, // num_heads or batch_size x num_heads
         const double p_dropout,
         const double softmax_scale,
         bool is_causal,
@@ -274,7 +274,7 @@ mha_fwd(at::Tensor &q,         // batch_size x seqlen_q x num_heads x head_size
         int64_t window_size_right,
         const double softcap,
         const bool return_softmax,
-        c10::optional<at::Generator> gen_) {
+        std::optional<at::Generator> gen_) {
 
     auto dprops = at::cuda::getCurrentDeviceProperties();
     // bool is_sm75 = dprops->major == 7 && dprops->minor == 5;
@@ -455,12 +455,12 @@ std::vector<at::Tensor>
 mha_varlen_fwd(at::Tensor &q,  // total_q x num_heads x head_size, total_q := \sum_{i=0}^{b} s_i
                const at::Tensor &k,  // total_k x num_heads_k x head_size, total_k := \sum_{i=0}^{b} s_i or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
                const at::Tensor &v,  // total_k x num_heads_k x head_size, total_k := \sum_{i=0}^{b} s_i or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
-               const c10::optional<at::Tensor> &out_, // total_q x num_heads x head_size, total_k := \sum_{i=0}^{b} s_i
+               const std::optional<at::Tensor> &out_, // total_q x num_heads x head_size, total_k := \sum_{i=0}^{b} s_i
                const at::Tensor &cu_seqlens_q,  // b+1
                const at::Tensor &cu_seqlens_k,  // b+1
-               const c10::optional<at::Tensor> &seqused_k, // b. If given, only this many elements of each batch element's keys are used.
-               const c10::optional<at::Tensor> &block_table_, // batch_size x max_num_blocks_per_seq
-               const c10::optional<at::Tensor> &alibi_slopes_, // num_heads or b x num_heads
+               const std::optional<at::Tensor> &seqused_k, // b. If given, only this many elements of each batch element's keys are used.
+               const std::optional<at::Tensor> &block_table_, // batch_size x max_num_blocks_per_seq
+               const std::optional<at::Tensor> &alibi_slopes_, // num_heads or b x num_heads
                int64_t max_seqlen_q,
                const int64_t max_seqlen_k,
                const double p_dropout,
@@ -471,7 +471,7 @@ mha_varlen_fwd(at::Tensor &q,  // total_q x num_heads x head_size, total_q := \s
                int64_t window_size_right,
                const double softcap,
                const bool return_softmax,
-               c10::optional<at::Generator> gen_) {
+               std::optional<at::Generator> gen_) {
 
     auto dprops = at::cuda::getCurrentDeviceProperties();
     // bool is_sm75 = dprops->major == 7 && dprops->minor == 5;
@@ -726,15 +726,15 @@ std::vector<at::Tensor>
 mha_fwd_kvcache(at::Tensor &q,                 // batch_size x seqlen_q x num_heads x head_size
                 const at::Tensor &kcache,            // batch_size_c x seqlen_k x num_heads_k x head_size or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
                 const at::Tensor &vcache,            // batch_size_c x seqlen_k x num_heads_k x head_size or num_blocks x page_block_size x num_heads_k x head_size if there's a block_table.
-                const c10::optional<at::Tensor> &k_, // batch_size x seqlen_knew x num_heads_k x head_size
-                const c10::optional<at::Tensor> &v_, // batch_size x seqlen_knew x num_heads_k x head_size
-                const c10::optional<at::Tensor> &seqlens_k_, // batch_size
-                const c10::optional<at::Tensor> &rotary_cos_, // seqlen_ro x (rotary_dim / 2)
-                const c10::optional<at::Tensor> &rotary_sin_, // seqlen_ro x (rotary_dim / 2)
-                const c10::optional<at::Tensor> &cache_batch_idx_, // indices to index into the KV cache
-                const c10::optional<at::Tensor> &block_table_, // batch_size x max_num_blocks_per_seq
-                const c10::optional<at::Tensor> &alibi_slopes_, // num_heads or batch_size x num_heads
-                const c10::optional<at::Tensor> &out_,             // batch_size x seqlen_q x num_heads x head_size
+                const std::optional<at::Tensor> &k_, // batch_size x seqlen_knew x num_heads_k x head_size
+                const std::optional<at::Tensor> &v_, // batch_size x seqlen_knew x num_heads_k x head_size
+                const std::optional<at::Tensor> &seqlens_k_, // batch_size
+                const std::optional<at::Tensor> &rotary_cos_, // seqlen_ro x (rotary_dim / 2)
+                const std::optional<at::Tensor> &rotary_sin_, // seqlen_ro x (rotary_dim / 2)
+                const std::optional<at::Tensor> &cache_batch_idx_, // indices to index into the KV cache
+                const std::optional<at::Tensor> &block_table_, // batch_size x max_num_blocks_per_seq
+                const std::optional<at::Tensor> &alibi_slopes_, // num_heads or batch_size x num_heads
+                const std::optional<at::Tensor> &out_,             // batch_size x seqlen_q x num_heads x head_size
                 const double softmax_scale,
                 bool is_causal,
                 int64_t window_size_left,

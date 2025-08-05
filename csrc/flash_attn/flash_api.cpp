429297e92a33d5b293d7d288ad474b675a57ec92
simon-mo <xmo@berkeley.edu>
2025-08-05 05:20:32 -0700

    revert files back

--- csrc/flash_attn/flash_api.cpp
+++ csrc/flash_attn/flash_api.cpp
@@ -533,8 +533,7 @@ mha_varlen_fwd(at::Tensor &q,  // total_q x num_heads x head_size, total_q := \s
                int window_size_right,
                const float softcap,
                const bool return_softmax,
-               std::optional<at::Generator> gen_,
-               std::optional<at::Tensor> s_aux) {  // Added new parameter
+               std::optional<at::Generator> gen_) {  // Added new parameter
 
     // Otherwise the kernel will be launched from cuda:0 device
     at::cuda::CUDAGuard device_guard{q.device()};
@@ -692,8 +691,7 @@ mha_varlen_fwd(at::Tensor &q,  // total_q x num_heads x head_size, total_q := \s
                      seqlenq_ngroups_swapped,
                      /*unpadded_lse*/true);
     params.total_q = total_q;
-    params.s_aux_ptr = s_aux.has_value() ? s_aux.value().data_ptr() : nullptr;
-
+        
     if (paged_KV) {
         params.block_table = block_table.data_ptr<int>();
         params.block_table_batch_stride = block_table.stride(0);

3ee3b55067c803e1b2c5bdcb807916a2422be5ec
Lucas Wilkinson <lwilkins@redhat.com>
2025-10-17 15:58:36 +0000

    more stable fix
    Signed-off-by: Lucas Wilkinson <lwilkins@redhat.com>

--- hopper/block.h
+++ hopper/block.h
@@ -40,8 +40,9 @@ struct BlockMN {
             // If local, blocking (m_idx_max - m_idx_min + window_size_right + window_size_left)  
             // when cp is not enabled, tot_seqlen_k is equal to seqlen_k, and cp_world_size is 1.
             // cp_world_size is guaranteed to be greater than 0
-            int n_token_max = m_idx_max + seqlen_k - seqlen_q + window_size_right;
-            if (seqlen_info.cp_world_size > 1) {
+            int tot_seqlen_k = (Is_local) ? seqlen_k : seqlen_info.tot_seqlen_k;
+            int n_token_max = m_idx_max + tot_seqlen_k - seqlen_q + window_size_right;
+            if (seqlen_info.cp_world_size > 1 && !Is_local) {
                 n_token_max = cute::ceil_div(n_token_max - seqlen_info.cp_rank, seqlen_info.cp_world_size);
             }
             n_block_max = std::min(n_block_max, cute::ceil_div(n_token_max, kBlockN));
